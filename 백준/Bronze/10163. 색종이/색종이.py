1eab6864969eac3f0e22e761948700d7934d79d6
kystic125 <120080427+kystic125@users.noreply.github.com>
2025-12-24 00:43:39 +0900

    [Bronze I] Title: 색종이, Time: 584 ms, Memory: 39264 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,23 +1,18 @@
 n = int(input())
-graph = [[0] * 1001 for _ in range(1001)]
-result = []
+graph = [[0] * 1010 for _ in range(1010)]
+result = [0] * (n+1)
 
 for k in range(1, n+1):
     a, b, c, d = map(int, input().split())
 
     for i in range(a, a+c):
-        for j in range(b, b+d):
-            graph[i][j] = k
+        graph[i][b:b+d] = [k] * d
 
 
-for i in range(1, n+1):
-    cnt = 0
-    for a in graph:
-        for b in a:
-            if b == i:
-                cnt += 1
+for i in range(1010):
+    for j in range(1010):
+        if graph[i][j] != 0:
+            result[graph[i][j]] += 1
 
-    result.append(cnt)
-
-for r in result:
-    print(r)
+for i in range(1, n + 1):
+    print(result[i])
